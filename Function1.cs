2c5d99da3e229134195e67e9bb931621a9760a14
Syed Hussain <syed.hussain@eax360.com>
2024-01-26 20:45:32 +0000

    push

--- Function1.cs
+++ Function1.cs
@@ -95,7 +95,7 @@ public static class CustomerFunction
 
         if (serviceBusClient != null)
         {
-            await serviceBusClient.CloseAsync();
+            await serviceBusClient.DisposeAsync();
         }
     }
 }
